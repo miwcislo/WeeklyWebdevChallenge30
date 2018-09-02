cacc8c223e9c336cbec70daa56f423af242d68e5
miwcislo <miwcislo@gmail.com>
2018-09-02 18:46:00 +0200

    fixed main.js file

--- js/main.js
+++ js/main.js
@@ -24,13 +24,7 @@ $(document).ready(function(){
   });
 });
 
-
-var slideUp = {
-    distance: '150%',
-    origin: 'bottom',
-    opacity: null
-};
-
+$(document).ready(function(){
 ScrollReveal().reveal('.box', { delay: 500, duration: 1000 });
 ScrollReveal().reveal('.content-1', { delay: 1000, duration: 1000  });
 ScrollReveal().reveal('.content-2', { delay: 1100, duration: 1000  });
@@ -38,3 +32,4 @@ ScrollReveal().reveal('.content-3', { delay: 1200, duration: 1000  });
 ScrollReveal().reveal('.content-4', { delay: 1300, duration: 1000  });
 ScrollReveal().reveal('.content-5', { delay: 1400, duration: 1000  });
 ScrollReveal().reveal('.content-6', { delay: 1500, duration: 1000  });
+});
